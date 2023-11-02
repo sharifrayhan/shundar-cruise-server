ed5f0cf2954c3c2f9d95cbb9ff1ad180ab96deb0
Your Name <you@example.com>
2023-11-02 22:04:16 +0600

    added get method for spots

--- index.js
+++ index.js
@@ -2,9 +2,61 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 var cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ujemn7v.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    const database = client.db('sundarcruiseDB')
+    const packageCollection = database.collection('packages')
+    const spotsCollection = database.collection('spots')
+
+
+    app.get('/spots', async(req,res)=>{
+        const cursor = spotsCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    app.get('/spots/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        console.log('i need data for id :', id);
+        const product =  await spotsCollection.findOne( query );
+        res.send(product);
+    })
+
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
